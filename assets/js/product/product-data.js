9dcd77691ae83f683d33d45f8b7c8449a86e8c30
vxphuc <phuc2652003@gmail.com>
2025-05-07 10:13:25 +0700

    push

--- assets/js/product/product-data.js
+++ assets/js/product/product-data.js
@@ -1,8 +1,8 @@
 const products = [
-    { name: "Yến sào chưng", category: "yen", image: "../../assets/img/anh/sữa tổ yến 2.png" },
-    { name: "Rong nho tách nước", category: "rong", image: "../../assets/img/anh/sữa tổ yến 2.png" },
-    { name: "Snack rong vị bơ tỏi", category: "snack", image: "../../assets/img/anh/sữa tổ yến 2.png" },
-    { name: "Yến chưng đương", category: "yen", image: "../../assets/img/anh/yến chưng dường phèn DT.png" },
-    { name: "Sữa tổ yến", category: "yen", image: "../../assets/img/anh/sữa tổ yến 1.png" },
+    { name: "Yến sào chưng", category: "yen", image: "/assets/img/anh/sữa tổ yến 2.png" },
+    { name: "Rong nho tách nước", category: "rong", image: "/assets/img/anh/sữa tổ yến 2.png" },
+    { name: "Snack rong vị bơ tỏi", category: "snack", image: "/assets/img/anh/sữa tổ yến 2.png" },
+    { name: "Yến chưng đương", category: "yen", image: "/assets/img/anh/yến chưng dường phèn DT.png" },
+    { name: "Sữa tổ yến", category: "yen", image: "/assets/img/anh/sữa tổ yến 1.png" },
   ];
   
