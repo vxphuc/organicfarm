dd9b48d50f7d3168bea9d95d8c2a6669d1521715
vxphuc <phuc2652003@gmail.com>
2025-06-23 08:17:05 +0700

    updatrr

--- assets/js/map/map.js
+++ assets/js/map/map.js
@@ -1,22 +1,24 @@
 const form = document.getElementById('contact-form');
 
-  form.addEventListener('submit', async function (e) {
-    e.preventDefault();
-    const data = new FormData(form);
-    const action = form.action;
-    const response = await fetch(action, {
-      method: 'POST',
-      body: data,
-      headers: {
-        'Accept': 'application/json'
-      }
-    });
+form.addEventListener('submit', async function (e) {
+  e.preventDefault();
 
-    if (response.ok) {
-      alert("Gửi liên hệ thành công!");
-      form.reset();
-      grecaptcha.reset();
-    } else {
-      alert("Có lỗi xảy ra. Vui lòng thử lại.");
+  const data = new FormData(form);
+
+  // Thay link bên dưới bằng mã Formspree của bạn
+  const response = await fetch('https://formspree.io/f/mnnvgayg', {
+    method: 'POST',
+    body: data,
+    headers: {
+      'Accept': 'application/json'
     }
   });
+
+  if (response.ok) {
+    alert("Gửi liên hệ thành công!");
+    form.reset();
+    grecaptcha.reset();
+  } else {
+    alert("Có lỗi xảy ra. Vui lòng thử lại.");
+  }
+});
