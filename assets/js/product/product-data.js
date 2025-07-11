d44938b44f37566be52fed9eed343efc79548d4e
vxphuc <phuc2652003@gmail.com>
2025-07-11 13:54:27 +0700

    update

--- assets/js/product/product-data.js
+++ assets/js/product/product-data.js
@@ -1,6 +1,33 @@
 const products = [
-    { name: "sinh khối trùn giống", category: "snack", image: "assets/img/anh/DSC01384.jpg" },
-    { name: "Trùn Thịt", category: "yen", image: "assets/img/anh/dowloand1.jpg" },
-    { name: "Phân Trùng Quế", category: "yen", image: "assets/img/anh/bannersup01.jpg" },
+    {
+    name: "Heo Rừng Lai",
+    category: "channuoi",
+    image: "/assets/img/anh/heo.jpg",
+    description: "Giống heo lai giữa heo rừng và heo nhà, cho thịt thơm ngon, ít mỡ."
+  },
+  {
+    name: "Sâu Canxi",
+    category: "trunque",
+    image: "/assets/img/anh/download.jpg",
+    description: "Dùng để nhân giống trùn quế, phù hợp cho mô hình nuôi tại nhà hoặc trang trại."
+  },
+  {
+    name: "sinh khối trùn giống",
+    category: "trunque",
+    image: "/assets/img/anh/DSC01384.jpg",
+    description: "Dùng để nhân giống trùn quế, phù hợp cho mô hình nuôi tại nhà hoặc trang trại."
+  },
+  {
+    name: "Trùn Thịt",
+    category: "trunque",
+    image: "/assets/img/anh/dowloand1.jpg",
+    description: "Trùn được nuôi để thu hoạch làm thức ăn giàu đạm cho gia súc, gia cầm, thủy sản."
+  },
+  {
+    name: "Phân Trùng Quế",
+    category: "trunque",
+    image: "/assets/img/anh/bannersup01.jpg",
+    description: "Phân hữu cơ từ trùn quế, giàu vi sinh, giúp cây trồng phát triển bền vững."
+  }
   ];
   
